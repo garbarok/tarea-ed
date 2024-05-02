f16c3068c3eb4435add1ddc9e24d3b451643801c
Óscar Gallego <oscar.gallego@checkinscan.com>
2024-05-02 17:46:32 +0200

    Mi segundo commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,7 +3,7 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Estamos haciendo mi tercer commit");
+		System.out.println("Estamos haciendo mi segundo commit");
 
 	}
 
