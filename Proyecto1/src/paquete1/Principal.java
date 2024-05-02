dcbe867e7c123159f100a038401e6cdde4bbd1c7
Oscar Gallego <info@oscargallegoruiz.com>
2024-05-02 17:54:55 +0200

    Este es un push de ejemplo

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,7 +3,7 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Estoy haciendo mi tercer commit en clase");
+		System.out.println("Estoy haciendo mi segundo push en clase");
 
 	}
 
